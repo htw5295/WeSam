0fcd82e92046e9565c592f8cd012d17e8ed9508e
HwangTaeWook <xowktktjd@gmail.com>
2017-08-07 17:38:54 +0900

    반응형 제작 시작 @media사용

--- js/Main.js
+++ js/Main.js
@@ -1,6 +1,10 @@
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
+function menuBar(x) {
+  x.classList.toggle("change");
+}
+
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("top").style.display = "block";
